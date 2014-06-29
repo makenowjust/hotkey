94d41006310c40dd5b02e42241c8c59d953d0782
MakeNowJust <make.just.on@gmail.com>
2014-06-29 13:20:15 +0900

    Add more wni32api's function for allowing for calling hotkey.Register after hotkey.Start

--- win/win.go
+++ win/win.go
@@ -13,19 +13,29 @@ const (
 )
 
 var (
-	libuser32 uintptr
+	libuser32   uintptr
+	libkernel32 uintptr
 
-	registerHotKey   uintptr
-	unregisterHotKey uintptr
+	registerHotKey    uintptr
+	unregisterHotKey  uintptr
+	postThreadMessage uintptr
+
+	getCurrentThread uintptr
+	getThreadId      uintptr
 )
 
 func init() {
 	// Library
 	libuser32 = MustLoadLibrary("user32.dll")
+	libkernel32 = MustLoadLibrary("kernel32.dll")
 
 	// Functions
 	registerHotKey = MustGetProcAddress(libuser32, "RegisterHotKey")
 	unregisterHotKey = MustGetProcAddress(libuser32, "UnregisterHotKey")
+	postThreadMessage = MustGetProcAddress(libuser32, "PostThreadMessageW")
+
+	getCurrentThread = MustGetProcAddress(libkernel32, "GetCurrentThread")
+	getThreadId = MustGetProcAddress(libkernel32, "GetThreadId")
 }
 
 func RegisterHotKey(hwnd HWND, id int32, fsModifiers, vk uint32) bool {
@@ -39,6 +49,16 @@ func RegisterHotKey(hwnd HWND, id int32, fsModifiers, vk uint32) bool {
 	return ret != 0
 }
 
+func PostThreadMessage(idThread uint32, msg uint32, wParam, lParam int32) bool {
+	ret, _, _ := syscall.Syscall6(postThreadMessage, 4,
+		uintptr(idThread),
+		uintptr(msg),
+		uintptr(wParam),
+		uintptr(lParam),
+		0, 0)
+	return ret != 0
+}
+
 func UnregisterHotKey(hwnd HWND, id int32) bool {
 	ret, _, _ := syscall.Syscall(unregisterHotKey, 2,
 		uintptr(hwnd),
@@ -46,4 +66,14 @@ func UnregisterHotKey(hwnd HWND, id int32) bool {
 		0)
 
 	return ret != 0
+}
+
+func GetCurrentThread() HANDLE {
+	ret, _, _ := syscall.Syscall(getCurrentThread, 0, 0, 0, 0)
+	return HANDLE(ret)
+}
+
+func GetThreadId(thread HANDLE) uint32 {
+	ret, _, _ := syscall.Syscall(getThreadId, 1, uintptr(thread), 0, 0)
+	return uint32(ret)
 }
